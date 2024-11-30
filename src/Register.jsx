a149a5cb47dbefcbbdbbb86d4bec6d98079e4e75
Ahnaf <mohammadahnaf123@gmail.com>
2024-11-30 11:17:02 +0400

    Create handleSubmit for both login and register forms
    Created handleSubmit and handleReset for both login and register forms
    Added fetch requests for both /login and /register routes
    Displayed proper error messages and data received from backend
    Added toggling of viewing passwords for both login and register forms

--- src/Register.jsx
+++ src/Register.jsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 function Register() {
   const [registerForm, setRegisterForm] = useState({
@@ -18,6 +19,12 @@ function Register() {
     password: null,
     confirmpassword: null,
   });
+
+  const [showPassword, setShowPassword] = useState({
+    password: false,
+    confirmpassword: false,
+  });
+  const navigate = useNavigate();
 
   function handleChange(e) {
     const { name, value } = e.target;
@@ -76,9 +83,89 @@ function Register() {
     }
   }
 
+  function handleShowPassword(field) {
+    setShowPassword((prev) => ({
+      ...prev,
+      [field]: !prev[field],
+    }));
+  }
+
+  function handleReset() {
+    setRegisterForm({
+      firstname: "",
+      lastname: "",
+      email: "",
+      username: "",
+      password: "",
+      confirmpassword: "",
+    });
+
+    setFormError({
+      firstname: null,
+      lastname: null,
+      email: null,
+      username: null,
+      password: null,
+      confirmpassword: null,
+    });
+  }
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    if (!e.target.checkValidity()) {
+      return;
+    }
+    const registerData = {
+      firstname: registerForm.firstname,
+      lastname: registerForm.lastname,
+      email: registerForm.email,
+      username: registerForm.username,
+      password: registerForm.password,
+      confirmpassword: registerForm.confirmpassword,
+    };
+
+    try {
+      const response = await fetch("http://localhost:3000/users/register", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(registerData),
+      });
+      if (!response.ok) {
+        // handle server form validation errors
+        const data = await response.json();
+        if (Array.isArray(data.error)) {
+          const errors = data.error.reduce((acc, error) => {
+            acc[error.param] = error.msg; // Match `param` to the field name
+            return acc;
+          }, {});
+          setFormError(errors);
+        } else {
+          // handling register errors
+          const field = data.error.includes("username") ? "username" : "email";
+          setFormError((prev) => ({
+            ...prev,
+            [field]: data.error,
+          }));
+        }
+        return;
+      }
+      const data = await response.json();
+
+      console.log("Register successful:", data);
+      // Handle success (redirect)
+      navigate("/login");
+    } catch (error) {
+      console.error("Error during register:", error);
+    }
+  }
+
   return (
     <main className="min-h-screen p-3 bg-gray-800 flex justify-center items-center">
       <form
+        onSubmit={(e) => handleSubmit(e)}
+        onReset={handleReset}
         noValidate
         action="#"
         className="bg-white p-3 rounded-md md:min-w-[50%] min-w-[85%] flex flex-col gap-5"
@@ -184,17 +271,80 @@ function Register() {
           <label className="font-custom font-bold" htmlFor="password">
             Password <span className="text-red-800">*</span>
           </label>
-          <input
-            className="border-black border-[3px] rounded-full px-2 py-1"
-            type="password"
-            id="password"
-            name="password"
-            value={registerForm.password}
-            placeholder="password"
-            minLength={6}
-            onChange={(e) => handleChange(e)}
-            required
-          />
+          <div className="relative flex items-center">
+            <input
+              className="border-black border-[3px] rounded-full px-2 flex-1 pr-14 py-1"
+              type={showPassword.password ? "text" : "password"}
+              id="password"
+              name="password"
+              value={registerForm.password}
+              placeholder="password"
+              minLength={6}
+              onChange={(e) => handleChange(e)}
+              required
+            />
+            <button
+              onClick={() => handleShowPassword("password")}
+              className="absolute right-[4%]"
+            >
+              {showPassword.password ? (
+                <svg
+                  className="size-7"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                  <g
+                    id="SVGRepo_tracerCarrier"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  ></g>
+                  <g id="SVGRepo_iconCarrier">
+                    {" "}
+                    <path
+                      d="M2.99902 3L20.999 21M9.8433 9.91364C9.32066 10.4536 8.99902 11.1892 8.99902 12C8.99902 13.6569 10.3422 15 11.999 15C12.8215 15 13.5667 14.669 14.1086 14.133M6.49902 6.64715C4.59972 7.90034 3.15305 9.78394 2.45703 12C3.73128 16.0571 7.52159 19 11.9992 19C13.9881 19 15.8414 18.4194 17.3988 17.4184M10.999 5.04939C11.328 5.01673 11.6617 5 11.9992 5C16.4769 5 20.2672 7.94291 21.5414 12C21.2607 12.894 20.8577 13.7338 20.3522 14.5"
+                      stroke="#000000"
+                      stroke-width="1.7759999999999998"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    ></path>{" "}
+                  </g>
+                </svg>
+              ) : (
+                <svg
+                  className="size-7"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                  <g
+                    id="SVGRepo_tracerCarrier"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  ></g>
+                  <g id="SVGRepo_iconCarrier">
+                    {" "}
+                    <path
+                      d="M15.0007 12C15.0007 13.6569 13.6576 15 12.0007 15C10.3439 15 9.00073 13.6569 9.00073 12C9.00073 10.3431 10.3439 9 12.0007 9C13.6576 9 15.0007 10.3431 15.0007 12Z"
+                      stroke="#000000"
+                      stroke-width="2"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    ></path>{" "}
+                    <path
+                      d="M12.0012 5C7.52354 5 3.73326 7.94288 2.45898 12C3.73324 16.0571 7.52354 19 12.0012 19C16.4788 19 20.2691 16.0571 21.5434 12C20.2691 7.94291 16.4788 5 12.0012 5Z"
+                      stroke="#000000"
+                      stroke-width="2"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    ></path>{" "}
+                  </g>
+                </svg>
+              )}
+            </button>
+          </div>
           <span className="text-red-600">{formError.password}</span>
         </div>
 
@@ -202,17 +352,81 @@ function Register() {
           <label className="font-custom font-bold" htmlFor="confirmpassword">
             Confirm Password <span className="text-red-800">*</span>
           </label>
-          <input
-            className="border-black border-[3px] rounded-full px-2 py-1"
-            type="password"
-            id="confirmpassword"
-            name="confirmpassword"
-            value={registerForm.confirmpassword}
-            placeholder="confirmpassword"
-            minLength={6}
-            onChange={(e) => handleChange(e)}
-            required
-          />
+          <div className="relative flex items-center">
+            <input
+              className="border-black border-[3px] rounded-full px-2 flex-1 pr-14 py-1"
+              type={showPassword.confirmpassword ? "text" : "password"}
+              id="confirmpassword"
+              name="confirmpassword"
+              value={registerForm.confirmpassword}
+              placeholder="confirmpassword"
+              minLength={6}
+              onChange={(e) => handleChange(e)}
+              required
+            />
+            <button
+              onClick={() => handleShowPassword("confirmpassword")}
+              className="absolute right-[4%]"
+            >
+              {showPassword.confirmpassword ? (
+                <svg
+                  className="size-7"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                  <g
+                    id="SVGRepo_tracerCarrier"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  ></g>
+                  <g id="SVGRepo_iconCarrier">
+                    {" "}
+                    <path
+                      d="M2.99902 3L20.999 21M9.8433 9.91364C9.32066 10.4536 8.99902 11.1892 8.99902 12C8.99902 13.6569 10.3422 15 11.999 15C12.8215 15 13.5667 14.669 14.1086 14.133M6.49902 6.64715C4.59972 7.90034 3.15305 9.78394 2.45703 12C3.73128 16.0571 7.52159 19 11.9992 19C13.9881 19 15.8414 18.4194 17.3988 17.4184M10.999 5.04939C11.328 5.01673 11.6617 5 11.9992 5C16.4769 5 20.2672 7.94291 21.5414 12C21.2607 12.894 20.8577 13.7338 20.3522 14.5"
+                      stroke="#000000"
+                      stroke-width="1.7759999999999998"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    ></path>{" "}
+                  </g>
+                </svg>
+              ) : (
+                <svg
+                  className="size-7"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                  <g
+                    id="SVGRepo_tracerCarrier"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  ></g>
+                  <g id="SVGRepo_iconCarrier">
+                    {" "}
+                    <path
+                      d="M15.0007 12C15.0007 13.6569 13.6576 15 12.0007 15C10.3439 15 9.00073 13.6569 9.00073 12C9.00073 10.3431 10.3439 9 12.0007 9C13.6576 9 15.0007 10.3431 15.0007 12Z"
+                      stroke="#000000"
+                      stroke-width="2"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    ></path>{" "}
+                    <path
+                      d="M12.0012 5C7.52354 5 3.73326 7.94288 2.45898 12C3.73324 16.0571 7.52354 19 12.0012 19C16.4788 19 20.2691 16.0571 21.5434 12C20.2691 7.94291 16.4788 5 12.0012 5Z"
+                      stroke="#000000"
+                      stroke-width="2"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    ></path>{" "}
+                  </g>
+                </svg>
+              )}
+            </button>
+          </div>
+
           <span className="text-red-600">{formError.confirmpassword}</span>
         </div>
 
@@ -227,10 +441,16 @@ function Register() {
         </div>
 
         <div className="flex justify-center gap-5">
-          <button className="px-3 py-2 rounded-full bg-green-500 font-custom font-bold border-2 border-green-500 flex-1 text-white hover:bg-white hover:text-black">
+          <button
+            type="submit"
+            className="px-3 py-2 rounded-full bg-green-500 font-custom font-bold border-2 border-green-500 flex-1 text-white hover:bg-white hover:text-black"
+          >
             Submit
           </button>
-          <button className="px-3 py-2 rounded-full bg-gray-500 font-custom font-bold border-2 border-gray-500 flex-1 text-white hover:bg-white hover:text-black">
+          <button
+            type="reset"
+            className="px-3 py-2 rounded-full bg-gray-500 font-custom font-bold border-2 border-gray-500 flex-1 text-white hover:bg-white hover:text-black"
+          >
             Clear
           </button>
         </div>
