7c372e598d5d7b450ee7e671ac726b10050c6800
Mohammad Ahnaf <mohammadahnaf123@gmail.com>
2025-03-25 22:26:39 +0400

    Stop socket autoconnect
    Connected socket when Chat component mounts

--- socket.js
+++ socket.js
@@ -1,6 +1,7 @@
 import io from "socket.io-client";
 
-const socket = io.connect("https://messaging-app-backend-p1g9.onrender.com");
-// const socket = io.connect("https://messaging-app-backend-p1g9.onrender.com");
+const socket = io("https://messaging-app-backend-p1g9.onrender.com", {
+  autoConnect: false,
+});
 
 export default socket;
